1ea276918876e45f4baf13677f5420361a7c9233
GingeHagler <peterrjames1978@outlook.com>
2023-02-07 14:42:12 +0000

    Fix padding on proncess cards

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Game Variables 
+ */
+
+const cards = document.querySelectorAll(.'princess-card');
+
+function flipCard() {
+    if 
+} 
+
+cards.forEach(card => card.addEventListener('click', flipCard));

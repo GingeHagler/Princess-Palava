ee0b1eb0a1d95a3bacbbdc2c51db92148528e389
GingeHagler <peterrjames1978@outlook.com>
2023-02-07 22:10:42 +0000

    Change game name to Princess Pair Picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,10 @@
-/**
- * Game Variables 
- */
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log(e);
+    cardGenerator();
+    board();
+});
 
-const card = document.getElementsByClassName("princess-card");
-
-card.addEventListener("click", flipCard);
-
-function flipCard() {
-    card.classList.toggle("flipCard");
+const cardGenerator = () => {
+    //generate the object
+    let cardData = 
 }
-
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
-
-function flipCard() {
-    if (lockboard) {return};
-    if (this === firstCard) {return};
-
-    this.clasList.add('flip');
-
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-
-        return;
-    }
-
-    secondCard = this;
-    checkForMatch();
-}
-
-function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-    isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-
-    resetBoard();
-}
-
-function unFlipCards() {
-    lockBoard = true;
-
-    setTimeout(() => {
-        firstCard.clasList.remove('flip');
-        secondCard.classList.remove('flip');
-
-        resetBoard();
-    
-    }, 1500);
-}
-
-function resetBoard() {
-    hasFlippedCard = lockBoard = false;
-    firstCard = secondCard = null;
-}
-
-// shuffle elements
-    cards.forEach(card => {
-        const randomPos = Math.floor(Math.random() * cards.length);
-        card.style.order = randomPos;
-    });
-
-cards.forEach(card => card.addEventListener('click', flipCard));
-

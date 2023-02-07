d1db077711de9ebd0c904ab49b2bfb8b79b311a1
GingeHagler <peterrjames1978@outlook.com>
2023-02-07 16:04:56 +0000

    Add shuffle elements to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,64 @@
  * Game Variables 
  */
 
-const cards = document.querySelectorAll(.'princess-card');
+const cards = document.querySelectorAll('princess-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    if 
-} 
+    if (lockboard) {return};
+    if (this === firstCard) {return};
+
+    this.clasList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unFlipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.clasList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    
+    }, 1500);
+}
+
+function resetBoard() {
+    hasFlippedCard = lockBoard = false;
+    firstCard = secondCard = null;
+}
+
+// shuffle elements
+    cards.forEach(card => {
+        const randomPos = Math.floor(Math.random() * cards.length);
+        card.style.order = randomPos;
+    });
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+

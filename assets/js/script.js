593b60e7945de40dc2e4927d0bf1354591d1f79a
GingeHagler <peterrjames1978@outlook.com>
2023-02-08 16:48:04 +0000

    Add new game button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ const closeBtn = document.getElementById("closeBtn");
 let gameOn = false;
 let perfectMatch = 0;
 let flippedCard = false;
-let lockBoard = false; 
+let lockBoard = false;
 let firstCard, secondCard;
 let moves = 0;
 let finalTime = "";
@@ -43,7 +43,7 @@ function flipCard() {
         gameOn = true;
         timer();
     }
-    if (lockBoard) return; 
+    if (lockBoard) return;
     if (this === firstCard) return;
 
     this.classList.add('flip');
@@ -55,7 +55,136 @@ function flipCard() {
         return;
     }
 
-    secondCard = this; 
+    secondCard = this;
 
     checkCardMatch();
+}
+
+/*
+To check if first card and second card match
+*/
+
+function checkCardMatch() {
+    let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+    if (isMatch) perfectMatch += 1;
+
+    if (isMatch) pairMatch();
+    else noMatch();
+
+    if (perfectMatch === MAX_MATCH) winGame();
+}
+
+// when cards are paired they can no longer be clicked
+
+function pairMatch() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+// board is locked if no match then cards reset
+
+function noMatch() {
+    lockBoard = true;
+
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 900);
+
+    addMove();
+})
+
+//move counter
+moves = 0;
+moveContainer.innerHTML = 0;
+
+function addMove() {
+    moves++;
+    moveContainer.innerHtml = moves;
+}
+
+//timer
+let time;
+let minutes = 0;
+let seconds = 0;
+let timeStart = false;
+timeContainer.innerHTML = "Time " + minutes + " : " + seconds;
+
+function timer() {
+    time = setInterval(function) {
+        seconds++;
+        if (seconds === 59) {
+            minutes++;
+            seconds = 0;
+        }
+        timeContainer.innerHTML = "Time " + minutes + " : " + seconds;
+    }, 1000);
+}
+
+function stopTime() {
+    clearInterval(time);
+}
+
+//reset all cards after every round
+
+function resetBoard() {
+    [flippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function winGame() {
+    stopTime();
+    showWinMessage();
+}
+
+// show winners message
+
+function showWinMessage() {
+    modal.style.display = "block";
+    finalTime = timeContainer.innerHTML;
+
+    document.getElementById("finalMove").innerHTML = moves;
+    document.getElementById("totalTime").innerHTML = finalTime;
+    reset();
+}
+
+window.onClick = function (event) {
+    if (event.target.id == 'close') {
+        document.getElementById('modal').style.display = "none";
+    }
+};
+
+// card shuffle
+
+function shuffle() {
+    cards.forEach(cards => {
+        let randomPosition = Math.floor(Math.random() * 16);
+        cards.style.order = randomPosition;
+    });
+}
+
+// new game button
+
+function reset() {
+    setTimeout(() => {
+        flippedCard = false;
+        [firstCard, secondCard] = [null, null];
+        stopTime();
+        gameOn = false;
+        timeStart = false;
+        seconds = 0;
+        minutes = 0;
+        timeContainer.innerHTML = "Timer 0.00";
+        moves = 0;
+        moveContainer.innerHTML = 0;
+        perfectMatch = 0;
+        cards.forEach(cardReset => cardReset.classList.remove('flip'));
+        shuffle();
+        cards.forEach(card => card.addEventListener('click', flipCard));
+
+    }, 500);
 }
